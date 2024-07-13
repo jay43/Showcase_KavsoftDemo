830fcc43885b87b31cbd7c4ac1227f0864055331
Prayag Gediya <prayag.work60@gmail.com>
2024-07-13 17:18:53 +0530

    Initial Commit

---
+++ ShowCaseView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShowCaseView
+//
+//  Created by Leo on 13/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShowCaseView/ShowCaseViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowCaseViewApp.swift
+//  ShowCaseView
+//
+//  Created by Leo on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ShowCaseViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
